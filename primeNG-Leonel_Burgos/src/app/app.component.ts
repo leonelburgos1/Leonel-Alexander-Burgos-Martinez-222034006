56437ffa91b00bc80d8f362f4879708acc5bf1df
Alexander <alezander.burgo@gmail.com>
2024-10-19 10:11:56 -0500

    avance

--- primeNG-Leonel_Burgos/src/app/app.component.ts
+++ primeNG-Leonel_Burgos/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { FormularioPlantilla1Component } from './formularios/formulario-plantilla1/formulario-plantilla1.component';
+import { PasswordModule } from 'primeng/password';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ButtonModule,FormularioPlantilla1Component,PasswordModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
