7eebf4faf7ed5ed84b0dab091d67afa843e7a2e1
leonelburgos1 <alezander.burgos@gmail.com>
2024-11-02 11:04:39 -0500

    avance

--- primeNG-Leonel_Burgos/src/app/components/login-test/login-test.component.ts
+++ primeNG-Leonel_Burgos/src/app/components/login-test/login-test.component.ts
@@ -1,23 +1,25 @@
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
 import { PasswordModule } from 'primeng/password';
+import { LoginService } from '../../services/login.service';
 
 
 @Component({
   selector: 'app-login-test',
   standalone: true,
-  imports: [ReactiveFormsModule, InputTextModule, ButtonModule, PasswordModule, CommonModule], //Modulos requeridos para el trabajo con formularios
+  imports: [ReactiveFormsModule, InputTextModule, ButtonModule, PasswordModule, CommonModule, HttpClientModule], //Modulos requeridos para el trabajo con formularios // importo el HttpClientModule
   templateUrl: './login-test.component.html',
   styleUrl: './login-test.component.css'
 })
 export class LoginTestComponent {
   userForm: FormGroup;
 
-  constructor(private fb: FormBuilder){
-    this.userForm = this.fb.group({
+  constructor(private fb: FormBuilder ,private loginService: LoginService){ // el LoginService es el servicio que se creo en donde esta definido el metodo login
+    this.userForm = this.fb.group({ 
 
       name: ['', Validators.required],
       email:['', [Validators.required, Validators.email]],
@@ -31,6 +33,10 @@ export class LoginTestComponent {
 
   onSubmit(){
     if(this.userForm.valid){
+      const {email,password}= this.userForm.value;
+      // el login es el que esta en el loginservice, esto quiere deir que los servicios se pueden hacer por
+      // medio del constructor que se tiene definido, todo porque lo defini arriba
+      this.loginService.login(email,password).subscribe(Response => {console.log("exitoso", Response)})
       console.log(this.userForm.value);
     }else{
       console.log('Formulario invalido');
--- primeNG-Leonel_Burgos/src/app/services/login.service.ts
+++ primeNG-Leonel_Burgos/src/app/services/login.service.ts
@@ -6,14 +6,28 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class LoginService {
-  apiUrl= "https://localhost:7128/api/Users";
+  apiUrl= "https://localhost:7128/api/Users"; // esta es la url base.
+
+  // Observable es un objeto que representa un flujo de datos que se pueden manejar 
+  // de forma asíncrona. Los Observables son parte de la biblioteca RxJS (Reactive Extensions for JavaScript) 
+  // y se utilizan principalmente para manejar eventos o flujos de datos que pueden ocurrir en el futuro, 
+  // como respuestas de peticiones HTTP, eventos del usuario - se controla con HttpClientModule
 
   constructor(private http:HttpClient) { }
 
   login(username: string, password: string): Observable<any>{ //tipo de dato Observable, observable es un arreglo que va a recibir los datos (username y password)
     const body={username,password}
     return this.http.post(`${this.apiUrl}/login`,{username,password})
-    
+
   }
 
+  register(username: string, password: string): Observable<any>{ //tipo de dato Observable, observable es un arreglo que va a recibir los datos (username y password)
+    const body={username,password}
+    return this.http.post(`${this.apiUrl}/register`,{username,password})
+
+  }
+ //puedo crear un solo servicio con 4 metodos.
+ //lo anterior es arquitectura de microservicios ahi es donde cada servicio tiene 1 sola responsabilidad
+ //en este caso este servicio va tener 2 responsabilidades la de login and register
+ // son varias responsabilidades pero que tegan similitudes entre ellas.
 }
