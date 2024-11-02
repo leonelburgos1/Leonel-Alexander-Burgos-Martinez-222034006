6ca8f9709e7b32db9ca6f600fe1483ea11663c49
Alexander <alezander.burgo@gmail.com>
2024-11-01 23:34:32 -0500

    finalización de formulario

--- PracticaCpt_Leonel_Burgos/src/app/app.component.ts
+++ PracticaCpt_Leonel_Burgos/src/app/app.component.ts
@@ -1,15 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 import { FormUserComponent } from "./components/form-user/form-user.component";
-
+import { FormManagerComponent } from './components/form-manager/form-manager.component';
+import { FormLicenseComponent } from './components/form-license/form-license.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormUserComponent],
+  imports: [RouterOutlet, FormUserComponent, FormManagerComponent, FormLicenseComponent, ButtonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'PracticaCpt_Leonel_Burgos';
+  title = 'PracticaCpt_fernandoguevara';
+
+  loading: boolean = false;
+
+  load() {
+      this.loading = true;
+      
+      setTimeout(() => {
+        this.loading = false;
+      }, 2000); 
+    }
 }
--- PracticaCpt_Leonel_Burgos/src/app/components/form-license/form-license.component.ts
+++ PracticaCpt_Leonel_Burgos/src/app/components/form-license/form-license.component.ts
@@ -1,12 +1,37 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { PrimeIcons } from 'primeng/api';
+import { Router } from '@angular/router';
+import { CalendarModule } from 'primeng/calendar';
+
 
 @Component({
   selector: 'app-form-license',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, InputTextModule, CommonModule,CalendarModule],
   templateUrl: './form-license.component.html',
   styleUrl: './form-license.component.css'
 })
 export class FormLicenseComponent {
+  licenseform: FormGroup;
 
-}
+  constructor(private fb: FormBuilder ,private router: Router){
+    this.licenseform = this.fb.group({
+    
+      fechaInicio:['',Validators.required],
+      fechaFinalizacion: [null],
+      textoDeNotas: ['',Validators.required]
+
+    })
+   }
+
+   onSubmit() {
+    if (this.licenseform.valid) {
+      console.log('Formulario Enviado', this.licenseform.value);
+      this.router.navigate(['/logintest']);
+    } else {
+      this.licenseform.markAllAsTouched();
+    }
+  }}
